dd11dbcfe053a6ce3a64a66789d1473c3425c0c5
BURAK KOCAK <burakkocak884@hotmail.com>
2018-11-26 03:33:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ for( var i = 0; i< players.length; i++){
 
 }
 return players
-function johnLennonFacts(facts){
+function johnLennonFacts(){
   var facts = ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",

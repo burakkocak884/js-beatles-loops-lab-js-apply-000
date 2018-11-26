21873b0343980e093c295b23a0fe10c9e9cd2d2c
BURAK KOCAK <burakkocak884@hotmail.com>
2018-11-26 03:31:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 function theBeatlesPlay(players, instruments){
 
 
-var allPlayers = []
+var players = []
 for( var i = 0; i< players.length; i++){
   allPlayers.push(players[i] + " plays " + instruments[i])
 
 }
-return allPlayers
-}
+return players
 function johnLennonFacts(){
   var facts = ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"]
+  "He hated the sound of his own voice",
+"foo","bar"]
   var newFacts = []
   var i = 0
   while(i < facts.length){
